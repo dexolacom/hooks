699006f2897de00dbe52e8349db787fdf1141ded
happy-cutman <happy.cutman@gmail.com>
2022-03-10 13:35:52 +0200

    EDIT | new build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,3 @@
-export declare const networkSwitcher: (hexChainId: string, web3: any) => Promise<void>;
-export declare const addTokenToMetamask: (web3: any, tokenAddress: string, tokenSymbol: string, tokenDecimals: number) => Promise<void>;
+export declare const networkSwitcher: (web3: any, chainId: number) => Promise<void>;
+export declare const addTokenToMetamask: (web3: any, tokenAddress: string, tokenSymbol?: string | undefined, tokenDecimals?: number | undefined) => Promise<void>;
 //# sourceMappingURL=index.d.ts.map
