ff436b287b38885a709272ad03ce8a3b2b72cdff
happy-cutman <happy.cutman@gmail.com>
2022-03-10 11:52:16 +0200

    ADD | converting chainId to hex

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ Contains networkSwitcher and addToken functions for metamask extension.
 `networkSwitcher` first argument is chainId in hexString
 
 ```
-    networkSwitcher('0x38', web3)
+    networkSwitcher(56, web3)
     addTokenToMetamask(web3, '0xae13d989dac2f0debff460ac112a837c89baa7cd', 'BNB', 18)
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export const networkSwitcher = async (hexChainId: string, web3: any) => {
+export const networkSwitcher = async (chainId: number, web3: any) => {
+  const hexChainId = `0x${chainId.toString(16)}`
   try {
     // @ts-ignore
     await web3.currentProvider.request({
@@ -11,6 +12,11 @@ export const networkSwitcher = async (hexChainId: string, web3: any) => {
 };
 
 export const addTokenToMetamask = async (web3: any, tokenAddress: string, tokenSymbol: string, tokenDecimals: number) => {
+  // const iContract = new web3.eth.Contract(abi as any, ADDRESSES_FACTORY[chainId][key].iAddress)
+  // const decimals = await iContract.methods.decimals().call()
+  // const contract = new web3.eth.Contract(abi as any, ADDRESSES_FACTORY[chainId][key].address)
+  // const symbol = await contract.methods.symbol().call()
+
   try {
     // @ts-ignore
     await web3.currentProvider.request({
