77252c9388ca8b020bee1b5e1dd3df6e9041af43
happy-cutman <happy.cutman@gmail.com>
2022-03-16 10:39:55 +0200

    ADD | new build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,4 @@
+export declare const numericInputValidator: (e: React.ChangeEvent<HTMLInputElement>, inputId: string) => void;
 export declare const networkSwitcher: (web3: any, chainId: number) => Promise<void>;
 export declare const addTokenToMetamask: (web3: any, tokenAddress: string, tokenSymbol?: string | undefined, tokenDecimals?: number | undefined) => Promise<void>;
 //# sourceMappingURL=index.d.ts.map
