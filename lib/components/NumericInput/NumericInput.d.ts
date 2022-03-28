27e1c1e9b0a6db49cb6a7dd06aea785b3d488cd0
happy-cutman <happy.cutman@gmail.com>
2022-03-28 10:14:50 +0300

    ADD | new build

--- lib/components/NumericInput/NumericInput.d.ts
+++ lib/components/NumericInput/NumericInput.d.ts
@@ -1,5 +1,4 @@
 import React from 'react';
 import { NumericInputProps } from '../types';
-declare const NumericInput: ({ value, onUserInput, placeholder, onMax, inputBackground, inputBorderRadius, inputFontSize, inputFontColor, inputFontWeight, placeholderColor, inputWidth, inputPadding, inputTextAlign, buttonBackground, buttonTextColor, buttonFontSize, buttonFontWeight, buttonPadding, buttonBorder }: NumericInputProps & Omit<React.HTMLProps<HTMLInputElement>, 'ref' | 'onChange' | 'as'>) => any;
-export default NumericInput;
+export declare const NumericInput: ({ value, onUserInput, placeholder, onMax, inputBackground, inputBorderRadius, inputFontSize, inputFontColor, inputFontWeight, placeholderColor, inputWidth, inputPadding, inputTextAlign, buttonBackground, buttonTextColor, buttonFontSize, buttonFontWeight, buttonPadding, buttonBorder }: NumericInputProps & Omit<React.HTMLProps<HTMLInputElement>, 'ref' | 'onChange' | 'as'>) => any;
 //# sourceMappingURL=NumericInput.d.ts.map
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,4 @@
+export { NumericInput } from './components/NumericInput/NumericInput';
 export declare const removeEFromNumber: (number: number) => string;
 export declare const networkSwitcher: (web3: any, chainId: number) => Promise<void>;
 export declare const addTokenToMetamask: (web3: any, tokenAddress: string, tokenSymbol?: string | undefined, tokenDecimals?: number | undefined) => Promise<void>;
