80ba03b3a03a026b42484f5ce7419a0aa0ca3e20
happy-cutman <happy.cutman@gmail.com>
2022-04-04 09:30:13 +0300

    ADD | new imports

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -19,3 +19,16 @@ export const addNewTokenInfo = (address: string, name: string, abi: any, chainId
   const newObj = {[`${name}`]: {address, name, abi}}
   Object.assign(obj, newObj)
 };
+
+export const stringTrim = (string:string, maxLength:number) => {
+  if (!string) return string;
+  if (maxLength < 1) return string;
+  if (string.length <= maxLength) return string;
+  if (maxLength === 1) return string.substring(0,1) + '...';
+
+  const midpoint = Math.ceil(string.length / 2);
+  const toRemove = string.length - maxLength;
+  const leftStrip = Math.ceil(toRemove/2);
+  const rightStrip = toRemove - leftStrip;
+  return `${string.substring(0, midpoint - leftStrip)}...${string.substring(midpoint + rightStrip)}`
+}
